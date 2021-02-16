cd2327e4b8f7a96980a6fa6338968ba34541d113
Sophia Seremetis <shseremetis@gmail.com>
2021-02-16 17:48:20 -0500

    first problem

---
+++ questions.js
@@ -0,0 +1,18 @@
+// 1. Factorials!
+
+// Return the factorial of the provided integer.
+
+// If the integer is represented with the letter n, a factorial is the product of all positive integers less than or equal to n.
+
+// Factorials are often represented with the shorthand notation n!
+
+// 5! = 1 * 2 * 3 * 4 * 5 = 120
+
+// e.g.
+
+// factorialize(5) // returns 120
+// Only integers greater than or equal to zero will be supplied to the function.
+
+function factorialize(num) {
+  // Well... welcome to the Unknown
+}
