5ac5e555a2b5a962f8120cd85abe854aa041d823
Sophia Seremetis <shseremetis@gmail.com>
2021-02-27 17:49:57 -0500

    finish fizzbuzz

--- questions.js
+++ questions.js
@@ -19,22 +19,41 @@ function factorialize(num) {
 
 // Write a program that prints the numbers from 1 to 100. But for multiples of three, print "Fizz" instead of the number, and for the multiples of five, print "Buzz". For numbers which are multiples of both three and five, print "FizzBuzz".
 
-function fizzBuzz() {
-let nums = []
+// function fizzBuzz() {
+// let nums = []
 
-for (let i = 1; i <= 100; i++) {
-  nums.push(i)
+// for (let i = 1; i <= 100; i++) {
+//   nums.push(i)
+// }
+
+
+
+// let byThree = (nums.filter((num) => num % 3 === 0)) 
+// let byFive = nums.filter(num => num % 5 === 0)
+// let byThreeAndFive = nums.filter((num) => (num % 3 === 0) && (num % 5 === 0))
+
+// console.log(byThree)
+
+// }
+
+
+
+
+function fizzBuzz(){
+  for (let i = 1; i <= 100; i++){
+    let output = ''
+    if (i % 5 == 0 && i % 3 == 0){
+    output += 'Fizz-Buzz' 
+    } else if (i % 3 == 0) {
+      output += 'Fizz'
+    } else if (i % 5 == 0) {
+      output += 'Buzz'
+    } else {
+      output += i
+    }
+    console.log(output)
+  }
 }
-
-
-
-let byThree = (nums.filter((num) => num % 3 === 0)) 
-let byFive = nums.filter(num => num % 5 === 0)
-let byThreeAndFive = nums.filter((num) => (num % 3 === 0) && (num % 5 === 0))
-
-console.log(byThree)
-
-}
-
-
+  
+  
 fizzBuzz()
